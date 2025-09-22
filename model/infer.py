6ba18ff0f2fb0a420e7fe213d24be75677ff22df
Prabal Aryal <aryalprabal1@gmail.com>
2025-09-22 11:46:11 +0545

    minor changes made again

--- model/infer.py
+++ model/infer.py
@@ -53,147 +53,151 @@
 # # if __name__ == "__main__":
 # #     main()
 
-import time
-import subprocess
-import requests
-import numpy as np
-import os
-from scipy.io import wavfile
+# import time
+# import subprocess
+# import requests
+# import numpy as np
+# import os
+# from scipy.io import wavfile
 
-def main():
-    #........................................................... API base URL............................
-    base_url = "http://127.0.0.1:8000"
+# def main():
+#     #........................................................... API base URL............................
+#     base_url = "http://127.0.0.1:8000"
 
-    # .................................................List of sentences to inference.....................
-    sentences = [
-"Het kan gebroken glas of een lekkage maar natuurlijk zijn er ook andere mogelijkheden",
-"Het kan gas of een ondergelopen kelder maar natuurlijk zijn er ook andere mogelijkheden."
-"Mechanisme-monteurs maken met minimale middelen maximale mogelijkheden mogelijk.",
-"Moeilijke mechanisme-managers manipuleren met mechanische maatwerk-magie.",
-"maar natuurlijk zijn er ook andere mogelijkheden",
-"maar er zijn natuurlijk ook andere mogelijkheden",
-"ik zie de champignons schuifelen richting de waswachine",
-"Scherpe schaduwen schuiven schuifelend over het schoolplein. Charmante chefs kiezen champignons voor chique chardonnay-chutney.",
-"testzin woorden waar ch wel als g wordt uitgesproken",
-"Testzinn woorden met ch die als sj worden uitgesproken",
-"Mag ik uw gegevens noteren?",
-"Betreft het een probleem in de eigen woning of algemene ruimte?",
-"Melding doorgeven aan de dienstdoende van Staedion",
-"Ga in de eerste melding verder en noteer in deze melding het meldingsnummer van die melding",
-"Melding op Geen fax email",
-"In welke ruimte is het probleem?",
-"Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de badkamer, balkon of berging maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
-"Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld het dak,de galerij of de gang maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
-"Wat is het probleem in de badkamer?",
-"Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de afvoer, elektra of het gas maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
-"Gaat het om een lekkage of een verstopping?",
-"Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit lekkage of verstopping.",
-"Wat is er verstopt?",
-"Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit wastafel, douche, wasmachine of toilet.",
-"Adviseer om de wastafel douche wasmachine niet te gebruiken men kan de volgende werkdag opnieuw bellen",
-"Is er een tweede toilet in de woning?",
-"Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit ja of nee.",
-"Adviseer het verstopte toilet niet te gebruiken en verwijs de bewoner door naar de volgende werkdag",
-"Bewoner kan contact opnemen met het ontstoppingsbedrijf.",
-"Wat is er lek?",
-"Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de kraan,toilet stortbak of afvoer maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+#     # .................................................List of sentences to inference.....................
+#     sentences = [
+# "Het kan gebroken glas of een lekkage maar natuurlijk zijn er ook andere mogelijkheden",
+# "Het kan gas of een ondergelopen kelder maar natuurlijk zijn er ook andere mogelijkheden."
+# "Mechanisme-monteurs maken met minimale middelen maximale mogelijkheden mogelijk.",
+# "Moeilijke mechanisme-managers manipuleren met mechanische maatwerk-magie.",
+# "maar natuurlijk zijn er ook andere mogelijkheden",
+# "maar er zijn natuurlijk ook andere mogelijkheden",
+# "ik zie de champignons schuifelen richting de waswachine",
+# "Scherpe schaduwen schuiven schuifelend over het schoolplein. Charmante chefs kiezen champignons voor chique chardonnay-chutney.",
+# "testzin woorden waar ch wel als g wordt uitgesproken",
+# "Testzinn woorden met ch die als sj worden uitgesproken",
+# "Mag ik uw gegevens noteren?",
+# "Betreft het een probleem in de eigen woning of algemene ruimte?",
+# "Melding doorgeven aan de dienstdoende van Staedion",
+# "Ga in de eerste melding verder en noteer in deze melding het meldingsnummer van die melding",
+# "Melding op Geen fax email",
+# "In welke ruimte is het probleem?",
+# "Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de badkamer, balkon of berging maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
+# "Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld het dak,de galerij of de gang maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+# "Wat is het probleem in de badkamer?",
+# "Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de afvoer, elektra of het gas maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+# "Gaat het om een lekkage of een verstopping?",
+# "Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit lekkage of verstopping.",
+# "Wat is er verstopt?",
+# "Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit wastafel, douche, wasmachine of toilet.",
+# "Adviseer om de wastafel douche wasmachine niet te gebruiken men kan de volgende werkdag opnieuw bellen",
+# "Is er een tweede toilet in de woning?",
+# "Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit ja of nee.",
+# "Adviseer het verstopte toilet niet te gebruiken en verwijs de bewoner door naar de volgende werkdag",
+# "Bewoner kan contact opnemen met het ontstoppingsbedrijf.",
+# "Wat is er lek?",
+# "Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de kraan,toilet stortbak of afvoer maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
 
-"Adviseer de kraan niet te gebruiken en adviseer de bewoner de volgende werkdag opnieuw te bellen of een mail te sturen.",
-"Adviseer de bewoner de stortbak af te sluiten doormiddel van het kraantje.",
-"Melding doorgeven aan de loodgieter",
-"Geen spoed adviseer de bewoner de wastafel of douche niet te gebruiken en de volgende werkdag te bellen",
-"Is dit gebeurd door eigen toedoen?",
-"Meldt dan de de kosten voor de bewoner zijn, melding doorgeven aan dienstdoende van Staedion",
-"melding doorgeven aan dienstdoende van Staedion",
-"Lukt het om de stortbak af te sluiten door gebruik van het kraantje?",
-"Adviseer de bewoner om de volgende werkdag te bellen of een mail te sturen",
-"Melding doorgeven aan de nooddienstmedewerker",
-"Wat is er defect?",
-"Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de toiletbril, kraan of douchekop maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
-"Geen spoed adviseer de bewoner om de volgende werkdag terug te bellen of een mail te sturen",
-"Geen spoed adviseer de bewoner om de volgende werkdag terug te bellen",
-"Wat is het probleem met de kraan?",
-"Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit kraan druppelt of kraan kan niet dichtgedraaid.",
-"Wat is het probleem op het balkon of dakterras?",
-"Veel voorkomende keuzes zijn bijvoorbeeld het hang en sluitwerk van de deur,gebroken glas of een lekkage maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
-"Melding doorgeven aan het ontstoppingsbedrijf",
-"Is er een gevaarlijke situatie ontstaan?",
-"Melding doorgeven aan het gekoppelde bedrijf (bouwkundig)",
-"Bewoner doorverwijzen naar de volgende werkdag",
-"Wat is het probleem in de berging?",
-"Veel voorkomende keuzes zijn bijvoorbeeld elektra, gas of een ondergelopen kelder maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
-"Melding doorgeven aan de firma Proper",
-"Wat is het probleem in de buitenruimte of tuin?",
-"Veel voorkomende keuzes zijn bijvoorbeeld losse tegels of een kapotte schutting maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
-"Kan de kraan afgesloten worden?",
-"melding doorgeven aan de gekoppelde loodgieter",
-"Wat is het probleem met het dak?",
-"Veel voorkomende keuzes zijn bijvoorbeeld en verstopte dakgoot, lekke dakgoot of een kapot dakluik kranen of ventilatie maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
-"Dit is de verantwoordelijkheid van de bewoner",
-"Bewoner doorverwijzen naar de eerstvolgende werkdag",
-"Betreft het een gevaarlijke situatie?",
-]
+# "Adviseer de kraan niet te gebruiken en adviseer de bewoner de volgende werkdag opnieuw te bellen of een mail te sturen.",
+# "Adviseer de bewoner de stortbak af te sluiten doormiddel van het kraantje.",
+# "Melding doorgeven aan de loodgieter",
+# "Geen spoed adviseer de bewoner de wastafel of douche niet te gebruiken en de volgende werkdag te bellen",
+# "Is dit gebeurd door eigen toedoen?",
+# "Meldt dan de de kosten voor de bewoner zijn, melding doorgeven aan dienstdoende van Staedion",
+# "melding doorgeven aan dienstdoende van Staedion",
+# "Lukt het om de stortbak af te sluiten door gebruik van het kraantje?",
+# "Adviseer de bewoner om de volgende werkdag te bellen of een mail te sturen",
+# "Melding doorgeven aan de nooddienstmedewerker",
+# "Wat is er defect?",
+# "Dit antwoord herken ik niet. Veel voorkomende keuzes zijn bijvoorbeeld de toiletbril, kraan of douchekop maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
+# "Geen spoed adviseer de bewoner om de volgende werkdag terug te bellen of een mail te sturen",
+# "Geen spoed adviseer de bewoner om de volgende werkdag terug te bellen",
+# "Wat is het probleem met de kraan?",
+# "Dit antwoord herken ik niet. Kunt u het antwoord nogmaals geven? Maak een keuze uit kraan druppelt of kraan kan niet dichtgedraaid.",
+# "Wat is het probleem op het balkon of dakterras?",
+# "Veel voorkomende keuzes zijn bijvoorbeeld het hang en sluitwerk van de deur,gebroken glas of een lekkage maar natuurlijk zijn er ook andere mogelijkheden.Kunt u het antwoord nogmaals geven?",
+# "Melding doorgeven aan het ontstoppingsbedrijf",
+# "Is er een gevaarlijke situatie ontstaan?",
+# "Melding doorgeven aan het gekoppelde bedrijf (bouwkundig)",
+# "Bewoner doorverwijzen naar de volgende werkdag",
+# "Wat is het probleem in de berging?",
+# "Veel voorkomende keuzes zijn bijvoorbeeld elektra, gas of een ondergelopen kelder maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+# "Melding doorgeven aan de firma Proper",
+# "Wat is het probleem in de buitenruimte of tuin?",
+# "Veel voorkomende keuzes zijn bijvoorbeeld losse tegels of een kapotte schutting maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+# "Kan de kraan afgesloten worden?",
+# "melding doorgeven aan de gekoppelde loodgieter",
+# "Wat is het probleem met het dak?",
+# "Veel voorkomende keuzes zijn bijvoorbeeld en verstopte dakgoot, lekke dakgoot of een kapot dakluik kranen of ventilatie maar natuurlijk zijn er ook andere mogelijkheden. Kunt u het antwoord nogmaals geven?",
+# "Dit is de verantwoordelijkheid van de bewoner",
+# "Bewoner doorverwijzen naar de eerstvolgende werkdag",
+# "Betreft het een gevaarlijke situatie?",
+# ]
 
-    language = "nl"
+#     language = "nl"
 
-    # .........................Create directory for saving audio......................
-    output_dir = "new_audio_outputs"
-    os.makedirs(output_dir, exist_ok=True)
+#     # .........................Create directory for saving audio......................
+#     output_dir = "new_audio_outputs"
+#     os.makedirs(output_dir, exist_ok=True)
 
-    print("Starting real-time inference & playback...")
-    t0 = time.time()
+#     print("Starting real-time inference & playback...")
+#     t0 = time.time()
 
-    # ..........................Launch ffplay subprocess for raw audio...................
-    ffplay_cmd = [
-        "ffplay",
-        "-f", "f32le",        # 32-bit float PCM
-        "-ar", "24000",       # sample rate
-        "-ac", "1",           # mono
-        "-nodisp",
-        "-autoexit",
-        "-loglevel", "quiet",
-        "pipe:0"              # read from stdin
-    ]
-    proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
+#     # ..........................Launch ffplay subprocess for raw audio...................
+#     ffplay_cmd = [
+#         "ffplay",
+#         "-f", "f32le",        # 32-bit float PCM
+#         "-ar", "24000",       # sample rate
+#         "-ac", "1",           # mono
+#         "-nodisp",
+#         "-autoexit",
+#         "-loglevel", "quiet",
+#         "pipe:0"              # read from stdin
+#     ]
+#     proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
 
-    for sentence in sentences:
-        print(f"Processing sentence: {sentence[:50]}...")
+#     for sentence in sentences:
+#         print(f"Processing sentence: {sentence[:50]}...")
 
-        # ...............................Accumulate audio data for this sentence...............
-        sentence_audio_data = b""
+#         # ...............................Accumulate audio data for this sentence...............
+#         sentence_audio_data = b""
 
-        # ..................................Stream inference from API for each sentence.........
-        response = requests.post(f"{base_url}/inference_stream", params={"text": sentence, "language": language}, stream=True)
-        if response.status_code != 200:
-            print("Error in API request")
-            return
+#         # ..................................Stream inference from API for each sentence.........
+#         response = requests.post(f"{base_url}/inference_stream", params={"text": sentence, "language": language}, stream=True)
+#         if response.status_code != 200:
+#             print("Error in API request")
+#             return
 
-        chunk_size = 4 * 24000 
-        for chunk_bytes in response.iter_content(chunk_size=chunk_size):
-            if chunk_bytes:
-                proc.stdin.write(chunk_bytes)
-                sentence_audio_data += chunk_bytes
+#         chunk_size = 4 * 24000 
+#         for chunk_bytes in response.iter_content(chunk_size=chunk_size):
+#             if chunk_bytes:
+#                 proc.stdin.write(chunk_bytes)
+#                 sentence_audio_data += chunk_bytes
 
-        # ...........................Save sentence audio after processing....................
-        if sentence_audio_data:
-            # Convert to float32 
-            audio_np = np.frombuffer(sentence_audio_data, dtype=np.float32)
+#         # ...........................Save sentence audio after processing....................
+#         if sentence_audio_data:
+#             # Convert to float32 
+#             audio_np = np.frombuffer(sentence_audio_data, dtype=np.float32)
         
-            filename = sentence.replace(' ', '_').replace(',', '').replace('.', '').replace('?', '').replace('!', '') + '.wav'
-            filepath = os.path.join(output_dir, filename)
+#             filename = sentence.replace(' ', '_').replace(',', '').replace('.', '').replace('?', '').replace('!', '') + '.wav'
+#             filepath = os.path.join(output_dir, filename)
             
-            wavfile.write(filepath, 24000, audio_np)
-            print(f"Audio saved as {filepath}.")
+#             wavfile.write(filepath, 24000, audio_np)
+#             print(f"Audio saved as {filepath}.")
         
         
 
-    # ...............Close stdin to signal ffplay to finish.............
-    proc.stdin.close()
-    proc.wait()
-    print("Finished playback.")
+#     # ...............Close stdin to signal ffplay to finish.............
+#     proc.stdin.close()
+#     proc.wait()
+#     print("Finished playback.")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+#........................for the main.py.....................................
+
+
+
 
 # import time
 # import subprocess
